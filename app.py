0ffb14e5732bef2652bc0f26cd7ae7cdd2de7e7c
sriram <159551306+sriram-65@users.noreply.github.com>
2025-10-21 18:03:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ from otp.otp import Otp
 from student_teacher.role_student import Role_Student
 from student_teacher.role_admin import Role_Admin
 from flask_cors import CORS
+from datetime import timedelta
 
 app = Flask(__name__)
 CORS(app , supports_credentials=True)
 
-app.secret_key = '@MAHAKALI@THUNAI'
-
+app.secret_key = '@MAHAKALZI@THUNAI'
+app.permanent_session_lifetime = timedelta(days=5)
 app.register_blueprint(Auth , url_prefix='/api/auth')
 app.register_blueprint(Otp , url_prefix='/api/otp')
 app.register_blueprint(Role_Student , url_prefix='/api')
