f883093f6a972af0c4d4b2a1c5d9c7829bd89971
sriram <159551306+sriram-65@users.noreply.github.com>
2025-11-01 17:08:46 +0530

    Update helper.py

--- utils/helper.py
+++ utils/helper.py
@@ -1,10 +1,20 @@
 from db.db import ADMIN_EMAILS
+import cloudinary
+import cloudinary.uploader
+
+cloudinary.config(
+     cloud_name="dbrmvywb0",
+    api_key="799647841433247",
+    api_secret="XLtCOYXxRTnjZqwaF2oFnQ0AK7k"
+)
+
 
 def JSON_Parser(mongodb_cursor):
     try:
         data = []
         for i in mongodb_cursor:
             i['_id'] = str(i['_id'])
+            i['created_at'] = str(i['created_at'])
             data.append(i)
         
         return data
@@ -24,4 +34,29 @@ def Check_Role(email):
         return {"yes":False}
 
 
+
+def Filter_Options(dep , year , gender , busno , date):
+    try:
+        vars = {"dep":dep , "year":year, "gender":gender , "date":date , "busno":busno}
+        query = {}
+        for key , value in vars.items():
+            if value == 'all':
+                pass
+            else:
+                query[key] = value
+        
+        return query
+    except:
+        return False
+
+
+def Upload_Profile(img_url):
+    try:
+        img = cloudinary.uploader.upload(img_url)
+        if img:
+            return img['secure_url']
+        else:
+            return False
+    except:
+        return False
 
